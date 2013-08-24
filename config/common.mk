bc2039360e5ebd27859ec7139002d9d47d018953
tonyp <tonyptonyp@gmail.com>
2013-08-24 02:40:43 +0200

    remove kowalski manager + dependencies

--- config/common.mk
+++ config/common.mk
@@ -175,13 +175,7 @@ PRODUCT_PACKAGES += \
 
 # tonyp additions
 PRODUCT_COPY_FILES +=  \
-    vendor/cm/prebuilt/common/apk/BarcodeScanner.apk:system/app/BarcodeScanner.apk \
-    vendor/cm/prebuilt/common/apk/KowalskiManager.apk:system/app/KowalskiManager.apk \
-    vendor/cm/prebuilt/common/etc/init.d/95kowalski:system/etc/init.d/95kowalski \
-    vendor/cm/prebuilt/common/lib/hw/power.tegra.so:system/lib/hw/power.tegra.so \
-    vendor/cm/prebuilt/common/xbin/alsa_amixer:system/xbin/alsa_amixer \
-    vendor/cm/prebuilt/common/xbin/iwconfig:system/xbin/iwconfig \
-    vendor/cm/prebuilt/common/xbin/iwlist:system/xbin/iwlist
+    vendor/cm/prebuilt/common/apk/BarcodeScanner.apk:system/app/BarcodeScanner.apk
 
 # Enable mini gapps
 MINI_GAPPS := true
