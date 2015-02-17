5a0074b7b41f7b37de2973f25f4dcc0a572eba1d
tonyp <tonyptonyp@gmail.com>
2015-02-17 02:36:47 +0100

    Revert "Ship precompiled netd"
    This reverts commit 68cb080e2f4ac114d0db68d5917e006c25b60dc8.

--- config/tonyp.mk
+++ config/tonyp.mk
@@ -7,10 +7,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/apk/PerformanceControl.apk:system/app/PerformanceControl.apk \
     vendor/cm/prebuilt/common/apk/BarcodeScanner.apk:system/app/BarcodeScanner.apk
 
-# Ship precompiled netd (compiled with GCC 4.7)
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/netd:system/bin/netd
-
 # Enable mini gapps
 MINI_GAPPS := true
 $(call inherit-product, vendor/google/tiny_gapps_nonneon_tonyp.mk)
